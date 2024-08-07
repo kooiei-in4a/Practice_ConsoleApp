45e223a4880888782e9154bb06ffe6d414d635be
IeKouichirou <ie.koichiro@bec-jp.co.jp>
2024-08-07 13:58:31 +0900

    課題取り組む直前まで完了

---
+++ 001_Pocker/src/Sample_Csharp/Pocker/Pocker/Deck.cs
@@ -0,0 +1,48 @@
+﻿using Pocker;
+using System;
+using System.Collections.Generic;
+
+namespace Pocker
+{
+    public class Deck
+    {
+        private List<Card> cards;
+
+        public Deck()
+        {
+            cards = new List<Card>();
+            string[] suits = { "H", "D", "C", "S" };
+            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+            foreach (string suit in suits)
+            {
+                foreach (string rank in ranks)
+                {
+                    cards.Add(new Card(suit, rank));
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random rng = new Random();
+            int n = cards.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                Card value = cards[k];
+                cards[k] = cards[n];
+                cards[n] = value;
+            }
+        }
+
+        public Card DealCard()
+        {
+            Card card = cards[cards.Count - 1];
+            cards.RemoveAt(cards.Count - 1);
+            return card;
+        }
+    }
+
+}
+
---
+++ 001_Pocker/src/Sample_Csharp/Pocker/Pocker/PokerGame.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pocker
+{
+    public class PokerGame
+    {
+        private Player player;
+        private Player computer;
+        private Deck deck;
+
+        public PokerGame(string playerName)
+        {
+            player = new Player(playerName);
+            computer = new Player("Computer");
+            deck = new Deck();
+            deck.Shuffle();
+        }
+
+        public void StartGame()
+        {
+            DealInitialCards();
+            PlayerTurn();
+            ComputerTurn();
+            DetermineWinner();
+        }
+
+        private void DealInitialCards()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                player.AddCard(deck.DealCard());
+                computer.AddCard(deck.DealCard());
+            }
+        }
+
+        private void PlayerTurn()
+        {
+            Console.WriteLine("Your hand:");
+            player.ShowHand();
+            Console.WriteLine("Enter the indices of the cards you want to exchange (0-4, separated by spaces):");
+            string[] indices = Console.ReadLine().Split(' ');
+            List<int> cardIndices = indices.Select(int.Parse).ToList();
+            player.ExchangeCards(deck, cardIndices);
+            Console.WriteLine("Your new hand:");
+            player.ShowHand();
+        }
+
+        private void ComputerTurn()
+        {
+            Random random = new Random();
+            int numCardsToExchange = random.Next(1, 4);
+            List<int> cardIndices = new List<int>();
+            for (int i = 0; i < numCardsToExchange; i++)
+            {
+                cardIndices.Add(random.Next(5));
+            }
+            computer.ExchangeCards(deck, cardIndices);
+        }
+
+        private void DetermineWinner()
+        {
+            // 手札の強さを比較して勝者を決定するロジックを実装
+            // ここでは簡単にランダムで勝者を決定します
+            Random random = new Random();
+            if (random.Next(2) == 0)
+            {
+                Console.WriteLine($"{player.Name} wins!");
+            }
+            else
+            {
+                Console.WriteLine($"{computer.Name} wins!");
+            }
+        }
+    }
+}

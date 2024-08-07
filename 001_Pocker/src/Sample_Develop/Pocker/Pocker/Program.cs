b9af769a1609e254ea234058a7244b4c6c968d76
IeKouichirou <ie.koichiro@bec-jp.co.jp>
2024-08-07 14:58:56 +0900

    ゲームの基本の流れを作成

--- 001_Pocker/src/Sample_Develop/Pocker/Pocker/Program.cs
+++ 001_Pocker/src/Sample_Develop/Pocker/Pocker/Program.cs
@@ -4,18 +4,74 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
             MainLogic();
-            Console.WriteLine("End  , World!");
         }
 
         static void MainLogic()
         {
-            // Todo:本来入力で受け取る ユーザーの名前
-            string userName = "Yamada Taro";
+            Console.WriteLine("********************************");
+            Console.WriteLine("Pocker Game Start !!");
+            Console.WriteLine("********************************");
+            Console.WriteLine("");
 
-            PokerGame game = new PokerGame(userName);
-            game.StartGame();
+            string userName = getUserName();
+            Console.WriteLine("こんにちは、" + userName + "さん！");
+
+            while (true) {
+               
+                Console.WriteLine("");
+                Console.WriteLine("*** START NEW GAME !! ***");
+                
+                PokerGame game = new PokerGame(userName);
+                game.StartGame();
+
+
+                if (isExit()) break;
+            }
+
+
+
+
+            Console.WriteLine("");
+            Console.WriteLine("********************************");
+            Console.WriteLine("Thanks for playing! See you later, " + userName + " !!");
+            Console.WriteLine("********************************");
+
+
+        }
+
+        private static string getUserName()
+        {
+            while (true)
+            {
+                Console.WriteLine("あなたの名前を入力してください。:");
+
+                string? userName = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(userName))
+                {
+                    return userName;
+                }
+            }
+        }
+
+        private static bool isExit()
+        {
+            Console.WriteLine("ゲームを終了しますか？[y or yes]");
+            string? userInput = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(userInput))
+            {
+                // 入力を小文字に変換して比較
+                string loweredInput = userInput.ToLower().Trim();
+
+                // Y, y, Yes, yes のいずれかの場合は true を返す
+                if (loweredInput == "y" || loweredInput == "yes")
+                {
+                    return true;
+                }
+            }
+
+            return false;
 
         }
 
